461b2d2f31da7fac252a9843ff851aeab4516d18
unknown <mohamed.ahmed8412000@gmail.com>
2022-06-05 19:30:58 +0300

    esecondtcommit

--- src/client.js
+++ src/client.js
@@ -1,16 +1,16 @@
-// import sanityClient from "@sanity/client";
-
-// export default sanityClient({
-//   projectId: "1spqvbhe",
-//   dataset: "production",
-//   useCdn: "true",
-// });
 import sanityClient from "@sanity/client";
 
 export default sanityClient({
   projectId: "1spqvbhe",
   dataset: "production",
-  apiVersion: "2021-08-31", // use a UTC date string
-  token: "sanity-auth-token", // or leave blank for unauthenticated usage
-  useCdn: true, // `false` if you want to ensure fresh data
+  useCdn: "true",
 });
+// import sanityClient from "@sanity/client";
+
+// export default sanityClient({
+//   projectId: "1spqvbhe",
+//   dataset: "production",
+//   apiVersion: "2021-08-31", // use a UTC date string
+//   token: "sanity-auth-token", // or leave blank for unauthenticated usage
+//   useCdn: true, // `false` if you want to ensure fresh data
+// });
